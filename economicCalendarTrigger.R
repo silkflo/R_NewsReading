d4bb8af6cc7aa6230e947cb6ebebeef86b6e7153
silkflo <florian.assous@hotmail.fr>
2020-11-05 23:29:08 +0700

    economic calendar add magic number
    for loop not break problem

--- economicCalendarTrigger.R
+++ economicCalendarTrigger.R
@@ -3,7 +3,7 @@ library(lubridate)
 library(tidyverse)
 library(stringr)
 library(magrittr)
-
+library(tidyverse)
 
 #--------------DATA IMPORT-----------------#
 DFevent <- try(read.csv("E:/trading/Git/R_NewsReading/calendar-event-list.csv"),silent = TRUE)
@@ -11,9 +11,12 @@ DFT2 <- try(read_csv("C:/Program Files (x86)/AM MT4 - Terminal 2/MQL4/files/Orde
                       col_names = c("MagicNumber", "TicketNumber", "OrderStartTime", "OrderCloseTime", "Profit", "Symbol", "OrderType"),
                       col_types = "iiccdci"),silent = TRUE)
 
+# get Magic Number and symbol
+DFT2MN <- DFT2 %>% group_by(MagicNumber) %>% select("MagicNumber","Symbol") %>% unique()
+
 #search for the trading currency to compare with the calendar
-SymbolTraded <- c(substr(DFT2 %$% Symbol %>% unique() %>% sort(),1,3),
-                  substr(DFT2 %$% Symbol %>% unique() %>% sort(),4,6))
+SymbolTraded <- c(substr(DFT2MN %$% Symbol %>% unique() %>% sort(),1,3),
+                  substr(DFT2MN %$% Symbol %>% unique() %>% sort(),4,6))
 #remove duplicate
 SymbolTraded <- SymbolTraded[!duplicated(SymbolTraded)]
 
@@ -34,55 +37,69 @@ DFeventResult$Start <- format(DFeventResult$Start, tz="Asia/Bangkok",usetz=TRUE)
 DFeventResult <- filter(DFeventResult, DFeventResult$Start > Sys.time() - 3600)
 
 #create a new column to compare the currency traded with the currency of the calendar
-duplicateCurency <- vector("numeric", nrow(DFeventResult))
-for(i in 1:nrow(DFeventResult)){
- # i <- 1
-  
-  for(j in 1: length(SymbolTraded)){
-    # j = 3
-         if(DFeventResult[i,4] == SymbolTraded[j]){
-            duplicateCurency[i] <- 1
-            break
-          } else{
-            duplicateCurency[i] <-0
-          }
-  }
- }
-
-#add new column telling us which currency we are currently trading
-DFeventResult$CurrencyTraded = duplicateCurency
-#filter the table with the currency traded true
-DFeventResult <- filter(DFeventResult,DFeventResult$CurrencyTraded == 1)
+#duplicateCurency <- vector("numeric", nrow(DFeventResult))
+#for(i in 1:nrow(DFeventResult)){
+# # i <- 1
+#  
+#  for(j in 1: length(SymbolTraded)){
+#    # j = 3
+#         if(DFeventResult[i,4] == SymbolTraded[j]){
+#            duplicateCurency[i] <- 1
+#            break
+#          } else{
+#            duplicateCurency[i] <-0
+#          }
+#  }
+# }
+#
+##add new column telling us which currency we are currently trading
+#DFeventResult$CurrencyTraded = duplicateCurency
+##filter the table with the currency traded true
+#DFeventResult <- filter(DFeventResult,DFeventResult$CurrencyTraded == 1)
 
 
 #-------FLAG RESULT--------------#
 # use for loop to compare all event time with current time to send us the signal
-for(i in nrow(DFeventResult)){
-  # i<-1
-  # 1 can be adjusted according how long before the event you want the signal stop the trading
-  if(difftime(DFeventResult$Start[i],Sys.time(), units = "hours") < 1){
-    flag <- 1
-    break
-  }else{
-    flag <- 0
-  }
+flag  <- vector("numeric", nrow(DFT2MN))
+
+# the 1st for loop will check any magic numbers
+for(i in nrow(DFT2MN)){
+ # i <-1
+
+    for(j in nrow(DFeventResult)){
+      # j<-1
+      # 1 can be adjusted according how long before the event you want the signal stop the trading
+      if(difftime(DFeventResult$Start[j],Sys.time(), units = "hours") < 1 &&
+          (substr(DFT2MN$Symbol[i],1,3) == DFeventResult$Currency[j] || substr(DFT2MN$Symbol[i],4,6) == DFeventResult$Currency[j] )){
+        flag[i] <- 1 
+        break
+      } else{
+        flag[i] <- 0
+      }
+      
+    }
   
 }
 
-print(flag)
+# add new column telling us which magic number shouldn't trade
+DFT2MN$Flag = flag
+ 
+# filter the table with magic number not allow to trade only
+DFT2MN <- filter(DFT2MN,DFT2MN$Flag == 1)
+
 
 
 #----------SEND SIGNAL IN A CSV FILE-------------#
 
 # write flag obtained to all terminals!
 #Terminal 1
-write.csv(flag, "C:/Program Files (x86)/AM MT4 - Terminal 1/MQL4/Files/01_MacroeconomicEvent.csv", row.names = F)
+write.csv(DFT2MN, "C:/Program Files (x86)/AM MT4 - Terminal 1/MQL4/Files/01_MacroeconomicEvent.csv", row.names = F)
 #Terminal 2
-write.csv(flag, "C:/Program Files (x86)/AM MT4 - Terminal 2/MQL4/Files/01_MacroeconomicEvent.csv", row.names = F)
+write.csv(DFT2MN, "C:/Program Files (x86)/AM MT4 - Terminal 2/MQL4/Files/01_MacroeconomicEvent.csv", row.names = F)
 #Terminal 3
-write.csv(flag, "C:/Program Files (x86)/AM MT4 - Terminal 3/MQL4/Files/01_MacroeconomicEvent.csv", row.names = F)
+write.csv(DFT2MN, "C:/Program Files (x86)/AM MT4 - Terminal 3/MQL4/Files/01_MacroeconomicEvent.csv", row.names = F)
 #Terminal 4
-write.csv(flag, "C:/Program Files (x86)/AM MT4 - Terminal 4/MQL4/Files/01_MacroeconomicEvent.csv", row.names = F)
+write.csv(DFT2MN, "C:/Program Files (x86)/AM MT4 - Terminal 4/MQL4/Files/01_MacroeconomicEvent.csv", row.names = F)
 
 
 
