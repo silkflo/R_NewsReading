dfdd9999c63e674c52b0d88595f261081338bf80
silkflo <florian.assous@hotmail.fr>
2020-11-06 19:29:43 +0700

    economicCalendarTrigger.R complete

---
+++ download_calendar.bat
@@ -0,0 +1,2 @@
+@echo off
+"C:\Program Files (x86)\MacroRecorder\MacroRecorder.exe" "-play=E:\trading\lazypop\Macro_01_11_2020 20-27-57.mrf"
--- economicCalendarTrigger.R
+++ economicCalendarTrigger.R
@@ -14,13 +14,6 @@ DFT2 <- try(read_csv("C:/Program Files (x86)/AM MT4 - Terminal 2/MQL4/files/Orde
 # get Magic Number and symbol
 DFT2MN <- DFT2 %>% group_by(MagicNumber) %>% select("MagicNumber","Symbol") %>% unique()
 
-#search for the trading currency to compare with the calendar
-SymbolTraded <- c(substr(DFT2MN %$% Symbol %>% unique() %>% sort(),1,3),
-                  substr(DFT2MN %$% Symbol %>% unique() %>% sort(),4,6))
-#remove duplicate
-SymbolTraded <- SymbolTraded[!duplicated(SymbolTraded)]
-
-
 #----------------TABLE EDITION--------------#
 # convert "Start to time format and filter high impact event
 DFeventResult <- data.frame(Start = as.POSIXct( DFevent$Start, format="%m/%d/%Y %H:%M:%S" , tz = "GMT"),
@@ -36,57 +29,33 @@ DFeventResult$Start <- format(DFeventResult$Start, tz="Asia/Bangkok",usetz=TRUE)
 #remove old events (can be adjusted according how long after the event have started you want keep the flag to 1)
 DFeventResult <- filter(DFeventResult, DFeventResult$Start > Sys.time() - 3600)
 
-#create a new column to compare the currency traded with the currency of the calendar
-#duplicateCurency <- vector("numeric", nrow(DFeventResult))
-#for(i in 1:nrow(DFeventResult)){
-# # i <- 1
-#  
-#  for(j in 1: length(SymbolTraded)){
-#    # j = 3
-#         if(DFeventResult[i,4] == SymbolTraded[j]){
-#            duplicateCurency[i] <- 1
-#            break
-#          } else{
-#            duplicateCurency[i] <-0
-#          }
-#  }
-# }
-#
-##add new column telling us which currency we are currently trading
-#DFeventResult$CurrencyTraded = duplicateCurency
-##filter the table with the currency traded true
-#DFeventResult <- filter(DFeventResult,DFeventResult$CurrencyTraded == 1)
-
 
 #-------FLAG RESULT--------------#
 # use for loop to compare all event time with current time to send us the signal
 flag  <- vector("numeric", nrow(DFT2MN))
 
 # the 1st for loop will check any magic numbers
-for(i in nrow(DFT2MN)){
+for(i in 1:nrow(DFT2MN)){
  # i <-1
 
-    for(j in nrow(DFeventResult)){
+    for(j in 1:nrow(DFeventResult)){
       # j<-1
       # 1 can be adjusted according how long before the event you want the signal stop the trading
-      if(difftime(DFeventResult$Start[j],Sys.time(), units = "hours") < 1 &&
+      if(as.double(difftime(DFeventResult$Start[j],Sys.time(), units = "hours")) < 1 &&
           (substr(DFT2MN$Symbol[i],1,3) == DFeventResult$Currency[j] || substr(DFT2MN$Symbol[i],4,6) == DFeventResult$Currency[j] )){
         flag[i] <- 1 
         break
       } else{
         flag[i] <- 0
       }
-      
     }
-  
 }
 
 # add new column telling us which magic number shouldn't trade
 DFT2MN$Flag = flag
  
 # filter the table with magic number not allow to trade only
-DFT2MN <- filter(DFT2MN,DFT2MN$Flag == 1)
-
+DFT2MN <-as.data.frame(DFT2MN) %>% filter(DFT2MN$Flag == 1)
 
 
 #----------SEND SIGNAL IN A CSV FILE-------------#
